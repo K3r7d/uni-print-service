c31b92960219bda87c23b87df076db0d55228ef8
Thành Minh <157796822+HCMUT-TaMi@users.noreply.github.com>
2024-11-29 14:32:49 +0700

    New Update for Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,30 @@
 import { memo } from 'react';
 import type { FC } from 'react';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import classes from './App.module.css';
 import resets from './components/_resets.module.css';
 import { HomePageLoginOffALL } from './components/HomePageLoginOffALL/HomePageLoginOffALL.js';
+import UserPage from './components/UserPage/user.js'
 
 interface Props {
   className?: string;
 }
 export const App: FC<Props> = memo(function App(props = {}) {
   return (
-    <div className={`${resets.clapyResets} ${classes.root}`}>
-      <HomePageLoginOffALL />
-    </div>
+    <BrowserRouter>
+      <div className={`${resets.clapyResets} ${classes.root}`}>
+        <Routes>
+          <Route path="/" element={<HomePageLoginOffALL />} />
+          <Route path="/user/:username" element={<UserPage />} />
+          <Route path="/admin/:username" element={<UserPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 });
